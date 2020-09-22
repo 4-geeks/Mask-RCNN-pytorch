32fa39155839936c0acec35b6f9c31ce13b9cbb5
amirhosseinh77 <amirhossein261077@live.com>
2020-09-22 18:12:21 +0330

    mask rcnn under development

---
+++ mask_rcnn.py
@@ -0,0 +1,85 @@
+###############################################
+# pytorch Mask-RCNN based on torchvision model
+# Amirhossein Heydarian
+###############################################
+
+import torch
+import torchvision
+from torchvision.models.detection.faster_rcnn import FastRCNNPredictor
+from torchvision.models.detection.mask_rcnn import MaskRCNNPredictor
+from torchvision.transforms import functional as F
+import cv2
+import numpy as np
+
+device = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
+num_classes = 11
+
+classes = {0:'_background_',
+           1:'cover',
+           2:'remote',
+           3:'pen',
+           4:'newspaper',
+           5:'scissors',
+           6:'kettle',
+           7:'cup',
+           8:'bottle',
+           9:'image',
+           10:'text'}
+
+font = cv2.FONT_HERSHEY_SIMPLEX 
+fontScale = 2
+thickness = 3
+
+
+def get_instance_segmentation_model(num_classes):
+    # load an instance segmentation model pre-trained on COCO
+    model = torchvision.models.detection.maskrcnn_resnet50_fpn()
+
+    # get the number of input features for the classifier
+    in_features = model.roi_heads.box_predictor.cls_score.in_features
+    # replace the pre-trained head with a new one
+    model.roi_heads.box_predictor = FastRCNNPredictor(in_features, num_classes)
+
+    # now get the number of input features for the mask classifier
+    in_features_mask = model.roi_heads.mask_predictor.conv5_mask.in_channels
+    hidden_layer = 256
+    # and replace the mask predictor with a new one
+    model.roi_heads.mask_predictor = MaskRCNNPredictor(in_features_mask,
+                                                       hidden_layer,
+                                                       num_classes)
+
+    return model
+
+class segmentation_model():
+    def __init__(self,model_path):
+        self.model = get_instance_segmentation_model(num_classes).to(device)
+        self.model.load_state_dict(torch.load(model_path))
+        self.model.eval()
+        
+    def detect_masks(self,image,rgb_image):
+        if not(rgb_image):
+          image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+        img = F.to_tensor(image)
+        with torch.no_grad():
+            prediction = self.model([img.to(device)])
+        return prediction[0]
+
+def plot_masks(image, prediction):
+    masks = prediction['masks'][:, 0].cpu().detach().numpy()[np.where(prediction['scores'].cpu().detach().numpy()>0.5)]
+    masks[masks<0.5] = 0
+    masks[masks>=0.5] = 1.0
+    labels = prediction['labels'].cpu().numpy()[np.where(prediction['scores'].cpu().detach().numpy()>0.5)]
+    scores = np.round(prediction['scores'].cpu().detach().numpy()[np.where(prediction['scores'].cpu().detach().numpy()>0.5)],2)
+
+    copy_image = image.copy()
+    alpha = 0.5
+    for i in range(masks.shape[0]):
+        color = (np.random.randint(255),np.random.randint(255),np.random.randint(255))
+        for c in range(3):
+            copy_image[:, :, c] = np.where(masks[i] == 1.0, copy_image[:, :, c] * (1 - alpha) + alpha*color[c], copy_image[:, :, c])
+
+        #adding classes   
+        args = np.where(masks[i]>0)
+        ymin,ymax,xmin,xmax = args[0].min(),args[0].max(),args[1].min(),args[1].max()
+        copy_image = cv2.putText(copy_image, '{} ({})'.format(classes[int(labels[i])],str(scores[i])), (xmin+10, ymin+10), font, fontScale, (0,0,0), thickness, cv2.LINE_AA)
+    return copy_image
---
+++ utils/dataset.py
@@ -0,0 +1,93 @@
+import os
+import numpy as np
+import torch
+import torch.utils.data
+import transforms as T
+
+
+def get_transform(train):
+    transforms = []
+    # converts the image, a PIL image, into a PyTorch Tensor
+    transforms.append(T.ToTensor())
+    if train:
+        # during training, randomly flip the training images
+        # and ground-truth for data augmentation
+        transforms.append(T.RandomHorizontalFlip(0.5))
+    return T.Compose(transforms)
+
+
+class maskrcnn_Dataset(torch.utils.data.Dataset):
+    def __init__(self, root, transforms=None):
+        self.root = root
+        self.transforms = transforms
+        # load all image files, sorting them to
+        # ensure that they are aligned
+        self.imgs = list(sorted(os.listdir(os.path.join(root, "JPEGImages"))))
+        self.masks = list(sorted(os.listdir(os.path.join(root, "SegmentationObject"))))
+        self.class_masks = list(sorted(os.listdir(os.path.join(root, "SegmentationClass"))))
+
+    def __getitem__(self, idx):
+        # load images ad masks
+        img_path = os.path.join(self.root, "JPEGImages", self.imgs[idx])
+        mask_path = os.path.join(self.root, "SegmentationObject", self.masks[idx])
+        class_mask_path = os.path.join(self.root, "SegmentationClass", self.class_masks[idx])
+        
+        #read and convert image to RGB
+        img = cv2.imread(img_path)
+        img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        # note that we haven't converted the mask to RGB,
+        # because each color corresponds to a different instance
+        # with 0 being background
+#         mask = Image.open(mask_path)
+        
+        mask = cv2.imread(mask_path,0)
+        class_mask = cv2.imread(class_mask_path,0)
+        # instances are encoded as different colors
+        obj_ids = np.unique(mask)
+        # first id is the background, so remove it
+        obj_ids = obj_ids[1:]
+
+        # split the color-encoded mask into a set
+        # of binary masks
+        masks = mask == obj_ids[:, None, None]
+
+        # get bounding box coordinates for each mask
+        num_objs = len(obj_ids)
+        boxes = []
+        for i in range(num_objs):
+            pos = np.where(masks[i])
+            xmin = np.min(pos[1])
+            xmax = np.max(pos[1])
+            ymin = np.min(pos[0])
+            ymax = np.max(pos[0])
+            boxes.append([xmin, ymin, xmax, ymax])
+
+        boxes = torch.as_tensor(boxes, dtype=torch.float32)
+        # there is only one class
+        labels = np.array([])
+        for i in range(masks.shape[0]):
+            labels = np.append(labels, (masks[i] * class_mask).max())
+        
+        labels = torch.as_tensor(labels, dtype=torch.int64)
+        masks = torch.as_tensor(masks, dtype=torch.uint8)
+
+        image_id = torch.tensor([idx])
+        area = (boxes[:, 3] - boxes[:, 1]) * (boxes[:, 2] - boxes[:, 0])
+        # suppose all instances are not crowd
+        iscrowd = torch.zeros((num_objs,), dtype=torch.int64)
+
+        target = {}
+        target["boxes"] = boxes
+        target["labels"] = labels
+        target["masks"] = masks
+        target["image_id"] = image_id
+        target["area"] = area
+        target["iscrowd"] = iscrowd
+
+        if self.transforms is not None:
+            img, target = self.transforms(img, target)
+
+        return img, target
+
+    def __len__(self):
+        return len(self.imgs)
