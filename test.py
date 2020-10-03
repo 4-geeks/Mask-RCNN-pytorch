82481be4683c1d5c05809e65e8365b40180e29ef
amirhosseinh77 <amirhossein261077@live.com>
2020-10-03 14:35:38 +0330

    test.py

--- test.py
+++ test.py
@@ -1,3 +1,15 @@
 import cv2
 import numpy as np
-import torch
+import matplotlib.pyplot as plt
+from mask_rcnn import *
+
+IMAGE_PATH = 'test_image.jpg'
+MODEL_PATH = './maskrcnn_saved_models/mask_rcnn_model_epoch_36.pt'
+
+image = cv2.imread(IMAGE_PATH)
+model = segmentation_model(MODEL_PATH)
+pred = model.detect_masks(image, rgb_image=False)   # rgb_image=False if loading image with cv2.imread
+
+plotted = plot_masks(image,pred)
+plt.figure(figsize=(16,12))
+plt.imshow(plotted)
