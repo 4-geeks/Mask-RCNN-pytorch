6f48da4680aaed1d98e86be2553b5cc9c60c3f4b
amirhosseinh77 <amirhossein261077@live.com>
2020-10-03 14:37:34 +0330

    fixed test.py

--- test.py
+++ test.py
@@ -8,7 +8,7 @@ MODEL_PATH = './maskrcnn_saved_models/mask_rcnn_model_epoch_36.pt'
 
 image = cv2.imread(IMAGE_PATH)
 model = segmentation_model(MODEL_PATH)
-pred = model.detect_masks(image, rgb_image=False)   # rgb_image=False if loading image with cv2.imread
+pred = model.detect_masks(image, rgb_image=False)   # rgb_image=False if loading image with cv2.imread()
 
 plotted = plot_masks(image,pred)
 plt.figure(figsize=(16,12))
